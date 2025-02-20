925f170e9a1045cb79cb8e34bbb04e547601afe4
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-02-20 06:49:59 +0300

    file io

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name) + ".txt"
+    with open(file_name, 'w') as lf:
+        lf.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name) + ".txt"  
+    with open(file_name, 'a') as lf:
+        lf.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name) + ".txt"  
+    with open(file_name, 'r') as lf:
+        return lf.read()
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+read_file(file_name="logfile")
